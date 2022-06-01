f23915b8195676e6f0f2b1beb4b45ad485bbf061
idoyudha <idowidya.yudhatama@gmail.com>
2022-06-01 23:02:54 +0700

    rest_error utils

---
+++ rest_errors/rest_errors_test.go
@@ -0,0 +1,33 @@
+package rest_errors
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewInternalServerError(t *testing.T) {
+	err := NewInternalServerError("test message", errors.New("database error"))
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusInternalServerError, err.Status)
+	assert.EqualValues(t, "test message", err.Message)
+	assert.EqualValues(t, "internal_server_error", err.Error)
+
+	assert.NotNil(t, err.Causes)
+	assert.EqualValues(t, 1, len(err.Causes))
+	assert.EqualValues(t, "database error", err.Causes[0])
+}
+
+func TestNewBadRequestError(t *testing.T) {
+	//TODO: Test!
+}
+
+func TestNewNotFoundrError(t *testing.T) {
+	//TODO: Test!
+}
+
+func TestNewError(t *testing.T) {
+	//TODO: Test!
+}
